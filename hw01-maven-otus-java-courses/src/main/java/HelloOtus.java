86a16db71cacafac18d025fada9f751d51a4e0cc
tyulalex <tyulalex@gmail.com>
2019-09-28 23:12:04 +0300

    delete test file

--- hw01-maven-otus-java-courses/src/main/java/HelloOtus.java
+++ hw01-maven-otus-java-courses/src/main/java/HelloOtus.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import com.google.common.collect.ImmutableList;
 
 public class HelloOtus {
+
+    public static void main(String[] args) {
+
+        ImmutableList<Integer> immutableList = ImmutableList.of(1, 2, 3, 4, 5, 4, 8);
+        for (Integer e : immutableList) {
+            System.out.println(e);
+        }
+    }
 }
