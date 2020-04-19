9f7f568630aa85a7584c65a6b79e81be7d59e1fe
tyulalex <tyulalex@gmail.com>
2020-04-19 22:40:38 +0300

    hw14 - numbers sequence

--- numbers-sequence/src/main/java/numbers/sequence/Main.java
+++ numbers-sequence/src/main/java/numbers/sequence/Main.java
@@ -1,27 +1,18 @@
 package numbers.sequence;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Main {
 
     private static final Integer MAX_PRINT_NUMBER = 10;
-    private static final List<String> THREAD_NAMES = List.of("PrinterThread1", "PrinterThread2");
-    private static Map<String, Integer> threadToPrintNumberForward = new HashMap<>();
-    private static Map<String, Integer> threadToPrintNumberBackward = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException {
         Main main = new Main();
         Thread one = new Thread(main::printNumbers);
-        one.setName(THREAD_NAMES.get(0));
+        one.setName("PrinterThread1");
         Thread two = new Thread(main::printNumbers);
-        two.setName(THREAD_NAMES.get(1));
-        threadToPrintNumberForward.put(one.getName(), 0);
-        threadToPrintNumberForward.put(two.getName(), 0);
-        threadToPrintNumberBackward.put(one.getName(), MAX_PRINT_NUMBER);
-        threadToPrintNumberBackward.put(two.getName(), MAX_PRINT_NUMBER);
+        two.setName("PrinterThread2");
         one.start();
         two.start();
         one.join();
@@ -30,38 +21,22 @@ public class Main {
 
     public synchronized void printNumbers() {
         String currentThreadName = Thread.currentThread().getName();
-        String otherThreadName = getOtherThreadName(currentThreadName);
-        for (int i = 1; i <= MAX_PRINT_NUMBER; i++) {
-            doPrintAndNotify(currentThreadName, i, threadToPrintNumberForward);
-            while (threadToPrintNumberForward.get(otherThreadName) < i) {
+        Supplier<IntStream> streamSupplier = () -> IntStream
+                .concat(
+                        IntStream.range(1, MAX_PRINT_NUMBER + 1),
+                        IntStream.range(2, MAX_PRINT_NUMBER)
+                                .map(i -> MAX_PRINT_NUMBER + 1 - i)
+                );
+        while (true) {
+            streamSupplier.get().forEach(i -> {
+                System.out.println(currentThreadName + " " + i);
+                notify();
                 try {
                     wait();
                 } catch (InterruptedException ex) {
 
                 }
-            }
+            });
         }
-        for (int j = MAX_PRINT_NUMBER - 1; j > 0; j--) {
-            doPrintAndNotify(currentThreadName, j, threadToPrintNumberBackward);
-            while (threadToPrintNumberBackward.get(otherThreadName) > j) {
-                try {
-                    wait();
-                } catch (InterruptedException ex) {
-
-                }
-            }
-        }
-    }
-
-    private String getOtherThreadName(String currentThreadName) {
-        Optional<String> optionalOtherThreadName = THREAD_NAMES.stream()
-                .filter(i -> !i.equals(currentThreadName)).findFirst();
-        return optionalOtherThreadName.orElseThrow();
-    }
-
-    private void doPrintAndNotify(String currentThreadName, int number, Map<String, Integer> sharedNotifyMap) {
-        System.out.println(currentThreadName + " " + number);
-        sharedNotifyMap.put(currentThreadName, number);
-        notify();
     }
 }
